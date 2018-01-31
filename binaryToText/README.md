cfe2b51512e19c54b6c9a191e14e8a6666b777ea
Kevin Southwick <d00230016@dmail.dixie.edu>
2018-01-31 15:05:02 -0700

    20180131

--- binaryToText/README.md
+++ binaryToText/README.md
@@ -1,3 +1,8 @@
 # Binary to Quotes Exercise
-Simple exercise where students are given a binary string and are to convert it to text. The list of texts are updatable. 
+Simple exercise where students are given a binary string and are to convert it to text. The list of texts are updatable.
+
+## Version
+* 20183101 - original version
+
+
 #### Programmed in JavaScript by Kevin Southwick for Dr. Gary Cantrell of Dixie State University and can be found at http://garycantrell.info/tutorials.php
--- binaryToText/js.js
+++ binaryToText/js.js
@@ -1,27 +1,136 @@
 //sets the quotes text file and supporting variables
 var textFile = "./quotes.txt";
-var lines;
+var lines = [];
+var numberOfLinesInFile;
+var currentLine = 0;
+
+
+
+
 
 //sets variables to HTML IDs
 var buttonBox = document.getElementById('buttonBox');
 var startButton = document.getElementById('startButton');
 var exerciseBox = document.getElementById('exerciseBox');
 var binaryBox = document.getElementById('binaryBox');
+var binaryText = document.getElementById('binaryText');
 var textBox = document.getElementById('textBox');
+var textInput = document.getElementById('textInput');
 var checkAnswerButton = document.getElementById('checkAnswerButton');
+var nextButton = document.getElementById('nextButton');
 
+
+
+
+
+//this is what happens when the page loads
 window.onload = function(){
+    //fetches the resource (the text file) and adds each line to an array (lines)
     fetch(textFile)
         .then(response => response.text())
         .then(text => lines = text.split("\n"));
 };
 
-console.log("hi");
-for(var i = 0; i < lines.length; i++){
-    console.log(lines[i]);
-}
+
+
+
+//give the page load a breather and then set these variables
+window.setTimeout(function(){
+    //sets the number of lines in the file for later use
+    numberOfLinesInFile = lines.length;
+    startButton.style.display="block";
+}, 3000)
+
+
+
+
+
 
 //this is what happens when the START button is clicked
 startButton.onclick = function(){
-    buttonBox.style.display = "none";
+    
+    console.log("Start. Current line: ", currentLine);
+    console.log("Start. Number of Lines in File: ", numberOfLinesInFile);
+    
+    if(currentLine > numberOfLinesInFile){
+        alert("Something went wrong. Please alert Professor Cantrell...");
+        return;
+    }else{
+        
+        //the START button disappears
+        buttonBox.style.display = 'none';
+
+        //exerciseBox appears with the first question
+        exerciseBox.style.display = 'block';
+
+        //binaryBox appears with the quote to translate
+        binaryBox.style.display = 'block';
+        binaryText.innerHTML = lines[currentLine];
+
+        //textBox appears with the input field
+        textBox.style.display = 'block';
+        textInput.style.display = 'inline';
+        textInput.placeholder = "Enter Answer";
+
+        //CHECK ANSWER button appears
+        checkAnswerButton.style.display = 'block';
+        
+        currentLine++;
+        
+        console.log("Start button hit. Current line: ", currentLine);
+        console.log("Start button hit. Number of Lines in File: ", numberOfLinesInFile);
+    }
+}
+
+
+
+
+
+//this is what happens when the CHECK ANSWER button is clicked
+checkAnswerButton.onclick = function(){
+    
+    //the CHECK ANSWER button disappears
+    checkAnswerButton.style.display = 'none';
+    
+    //binaryBox changes the contents to the answer
+    binaryText.innerHTML = lines[currentLine];
+    
+    //the NEXT button appears
+    nextButton.style.display = 'block';
+    
+    currentLine++;
+    
+    console.log("Check button hit. Current line: ", currentLine);
+    console.log("Check button hit. Number of Lines in File: ", numberOfLinesInFile);
+}
+
+
+
+
+
+//this is what happens when the NEXT button is clicked
+nextButton.onclick = function(){
+    
+    console.log("Next button hit. Current line: ", currentLine);
+    console.log("Next button hit. Number of Lines in File: ", numberOfLinesInFile);
+    
+    if(currentLine >= numberOfLinesInFile){
+        currentLine = 0;
+    }
+    
+    //the NEXT button disappears
+    nextButton.style.display = 'none';
+    //textInput resets input
+    textInput.value = "";
+    
+    //the CHECK ANSWER button appears
+    checkAnswerButton.style.display = 'block';
+    
+    //binaryBox changes the contents to the next question
+     binaryText.innerHTML = lines[currentLine];
+    
+    currentLine++;
+
+    console.log("Finish. Current line: ", currentLine);
+    console.log("Finish. Number of Lines in File: ", numberOfLinesInFile);
 }
